f0d819e68ad45eeef5523a40440722f4bbddd2fe
Himalee <himalee.tailor@gmail.com>
2018-11-27 10:34:48 +0000

    Refactor: create constant for empty string

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -2,22 +2,24 @@ import java.util.ArrayList;
 
 public class Wrapper {
     public static final String NEWLINE = "\n";
+    public static final String EMPTYSTRING = "";
     public static final int FIRSTELEMENT = 0;
 
     public static String wrap(String word, int columnLength) {
         ArrayList<String> wordChunks = new ArrayList<>();
+
         String remainingWord = word;
 
         while (remainingWord.length() > columnLength) {
             String splitWord = remainingWord.substring(FIRSTELEMENT, columnLength);
             wordChunks.add(splitWord);
-            remainingWord = remainingWord.replace(splitWord, "");
+            remainingWord = remainingWord.replace(splitWord, EMPTYSTRING);
 
         }
+
         wordChunks.add(remainingWord);
 
-      return  String.join(NEWLINE, wordChunks);
+        return String.join(NEWLINE, wordChunks);
 
     }
-
 }
